068fd692a160659b26b657bde50abc75326b1018
Nel-star <nelimadbaraza@gmail.com>
2022-09-08 23:50:37 -0700

    size of using printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main (entry point)
+ *
+ * Return 0 (always 0 for this code)
+ */
+int main (void)
+{
+	int a;
+	long int b;
+       	long long int c;
+	float d;
+	char e;
+
+	printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
+	return (0);
+}	
